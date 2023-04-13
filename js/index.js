dfa9d3a30c3bfea0db1d80437af85f85ce72cf69
Clementain <clentecarmona@gmail.com>
2023-04-13 18:43:28 -0500

    Se aplico estilos y responsividad, falta la tabla generada de los elementos ocultos de la tabla

--- js/index.js
+++ js/index.js
@@ -31,12 +31,30 @@ $(document).ready(() => {
                 }
             },
 
-        ],
-        responsive: true,
-        // columnDefs: [
-        //     { responsivePriority: 1, targets: [1, 2, 3] },
-        //     { responsivePriority: 2, targets: -1 },
-        // ]
+        ], "order": [[1, 'asc'], [2, 'asc']],
+        responsive: {
+            details: {
+                renderer: function (api, rowIdx, columns) {
+                    var data = $.map(columns, function (col, i) {
+                        return col.hidden ?
+                            '<tr data-dt-row="' + col.rowIndex + '" data-dt-column="' + col.columnIndex + '">' +
+                            '<td>' + col.title + ':' + '</td> ' +
+                            '<td>' + col.data + '</td>' +
+                            '</tr>' :
+                            '';
+                    }).join('');
+
+                    return data ?
+                        $('<table/>').append(data) :
+                        false;
+                }
+                //display: $.fn.dataTable.Responsive.display.modal()
+            }
+        },
+        columnDefs: [
+            { responsivePriority: 1, targets: [0, 1, 2, 3, 12, 13, 14, 15, 16] },
+            { responsivePriority: 2, targets: -1 },
+        ]
     });
 });
 
@@ -57,7 +75,7 @@ function editar(id) {
     window.location.href = "./FormularioRegistro.html?id=" + id;
 }
 
-function agregarEmpleado(){
+function agregarEmpleado() {
     window.location.replace("./FormularioRegistro.html");
 
 }
